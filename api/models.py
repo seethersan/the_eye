37586143e82de8a564f93b92fee39939c826e32b
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2022-02-23 04:27:51 -0500

    feature: added api models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Application(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    
+class Session(models.Model):
+    id = models.UUIDField(primary_key=True)
+
+class Event(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    category = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    data = models.JSONField()
+    timestamp = models.DateTimeField()
